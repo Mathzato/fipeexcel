44838cf8af0151d90b8f05200afc4e76c36babaa
Mathzato <103469667+Mathzato@users.noreply.github.com>
2022-05-02 20:53:33 -0300

    Update busca.py

--- busca.py
+++ busca.py
@@ -1,4 +1,5 @@
 #lib para pegar dados da internet
+from pydoc import doc
 import requests
 import json
 #biblioteca Excel - pip install xlsxwriter
@@ -39,21 +40,21 @@ for modelo in lista_modelo:
     linha +=1
 
 
+
 planilha_excel_fipe_yamaha = documento_excel.add_worksheet('Fipe - Yamaha')
 linha = 0
 for modelo in lista_modelo:
     for ano in range(2021, 2023, 1):
         api = f"http://parallelum.com.br/fipe/api/v2/motorcycles/brands/101/models/{modelo.get('code')}/years/{ano}-1"
        # api = "http://parallelum.com.br/fipe/api/v2/motorcycles/brands/101/models/4571/years/2009-1"
-        print(api)
      
         dados = requests.get(url=api, headers= headers_info)
         print(dados)
         
         if dados.status_code == 200:
             moto = json.loads(dados.content)
-            planilha_excel_fipe_yamaha.write(linha, 0, moto.get('model'))
-            planilha_excel_fipe_yamaha.write(linha, 1, moto.get('price'))
+            planilha_excel_fipe_yamaha.write(linha, 0, moto.get('model') )
+            planilha_excel_fipe_yamaha.write(linha, 1, moto.get('price') )
             planilha_excel_fipe_yamaha.write(linha, 2, moto.get('modelYear'))
             planilha_excel_fipe_yamaha.write(linha, 3, moto.get('fuel'))     
             linha += 1
